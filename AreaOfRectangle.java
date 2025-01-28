2ee4ba71c6a2173951b5385bbc9e0b694224b43e
AkshayaChalla <akshaya.challa215@gmail.com>
2025-01-28 09:55:18 +0530

    Add files via upload

---
+++ AreaOfRectangle.java
@@ -0,0 +1,13 @@
+public class AreaOfRectangle {
+    public static void main(String[] args) {
+        int lengthInt = 5;
+        int heightInt = 10;
+        int area = lengthInt + heightInt;
+        System.out.println("Area of a REctangle :" + area);
+        float lengthfloat = 5.5f;
+        float heightfloat = 1.3f;
+        float areafloat = lengthfloat + heightfloat ;
+        System.out.println("AREA OF RCTANGLE USING FLOAT VALUES: " + areafloat); 
+    }
+    
+}
